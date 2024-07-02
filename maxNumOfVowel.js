98ea9a173c7c6404c76905403547ce6ab6e809d5
Aniede Chibundu <ndubest56@gmail.com>
2024-07-02 03:21:02 +0100

    more on max num of vowels

--- maxNumOfVowel.js
+++ maxNumOfVowel.js
@@ -1,3 +1,6 @@
+//return the maximum number of consecutive vowels less than or equals the k in the string
+//Greedy Algorithm
+
 const maxVowel = (str, k) => {
   let newStr = "";
   let vowel = "aeiou";
@@ -23,5 +26,53 @@ const maxVowel = (str, k) => {
   }
   return num;
 };
-
 console.log(maxVowel("abciiideeeeef", 3));
+
+//Uptimum solution
+const maxNumOfStr = (str, k) => {
+  let vowels = "aeiou";
+  let count = 0;
+  let num = 0;
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+    if (i - start + 1 > k) {
+      if (vowels.includes(str[start])) {
+        count--;
+      }
+      start++;
+    }
+    num = Math.max(num, count);
+    if (num === k) {
+      return num;
+    }
+  }
+  return num;
+};
+console.log(maxNumOfStr("abciiideeeeef", 3));
+
+//More Efficent solution
+const maxNmOfVowel = (str, k) => {
+  let vowels = "aeiou";
+  let count = 0;
+  let num = 0;
+  let start = 0;
+
+  for (let i = 0; i < k; i++) {
+    if (vowels.includes(str[i])) count++;
+    if (count === k) return count + "count"; // early return 
+  }
+  for (let i = k; i < str.length; i++) {
+    if (vowels.includes(str[i - k])) count--;
+    if (vowels.includes(str[i])) count++;
+    num = Math.max(num, count);
+    
+    if (num === k) return num; // early return
+  }
+  return num + " num";
+};
+
+console.log(maxNmOfVowel("abciiideeeeef", 3));
