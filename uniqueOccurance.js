bb8668858f727d11494396e74522f65803de93d0
Aniede Chibundu <ndubest56@gmail.com>
2024-07-05 13:43:54 +0100

    diff between two arrays

--- uniqueOccurance.js
+++ uniqueOccurance.js
@@ -3,7 +3,7 @@ const uniqueCount = (nums) => {
   nums = nums.sort((a, b) => a - b);
   let counts = [];
 
-  console.log(nums);
+  //   console.log(nums);
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== nums[i + 1]) {
       count++;
@@ -19,3 +19,30 @@ const uniqueCount = (nums) => {
 console.log(uniqueCount([1, 2, 2, 1, 1, 3]));
 console.log(uniqueCount([1, 2]));
 console.log(uniqueCount([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]));
+
+// Using hashMap for better efficiency
+const uniqueCount1 = (nums) => {
+  let hashMap = new Map();
+  let numSet = new Set();
+
+  for (let i of nums) {
+    if (hashMap.has(i)) {
+      let count = hashMap.get(i);
+      hashMap.set(i, count + 1);
+    } else {
+      hashMap.set(i, 1);
+    }
+  }
+  for (let count of hashMap.values()) {
+    if (numSet.has(count)) {
+      return false;
+    } else {
+      numSet.add(count);
+    }
+  }
+  return true;
+};
+console.log("--------------------------");
+console.log(uniqueCount1([1, 2, 2, 1, 1, 3]));
+console.log(uniqueCount1([1, 2]));
+console.log(uniqueCount1([-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]));
