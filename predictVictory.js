e96a612e5296a278f4d704cfa02d317f595e3e37
Aniede Chibundu <ndubest56@gmail.com>
2024-07-08 17:58:33 +0100

    predict victory

---
+++ predictVictory.js
@@ -0,0 +1,25 @@
+const predictVictory = (senate) => {
+  let radient = [];
+  let dire = [];
+  let n = senate.length;
+
+  for (let i = 0; i < n; i++) {
+    if (senate[i] === `R`) {
+      radient.push(i + n);
+    } else {
+      dire.push(i + n);
+    }
+  }
+  while (radient.length > 0 && dire.length > 0) {
+    if (radient[0] < dire[0]) {
+      radient.push(radient[0] + n);
+    } else {
+      dire.push(dire[0] + n);
+    }
+    radient.shift();
+    dire.shift();
+  }
+  return { radient, dire };
+};
+
+console.log(predictVictory(["R", "R", "R", "D", "D", "D", "D"]));
