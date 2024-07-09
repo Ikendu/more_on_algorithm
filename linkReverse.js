7ffb194684eaaa0c6528e229f3d53cc3451ad8c2
Aniede Chibundu <ndubest56@gmail.com>
2024-07-09 08:25:57 +0100

    reverse linked list

---
+++ linkReverse.js
@@ -0,0 +1,13 @@
+//Not yet working well
+function linkReverse(head) {
+  let node = null;
+
+  while (head) {
+    let temp = head.next;
+    head.next = node;
+    node = head;
+    head = temp;
+  }
+
+  return node;
+}
