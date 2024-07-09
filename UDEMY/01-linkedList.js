f71de9fcd7ea9fc23a40bdcb0f8d287af4a3a659
Aniede Chibundu <ndubest56@gmail.com>
2024-07-09 14:53:11 +0100

    add to node

--- UDEMY/01-linkedList.js
+++ UDEMY/01-linkedList.js
@@ -12,6 +12,21 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 let myLinkedList = new LinkedList(4);
+myLinkedList.push(10);
+myLinkedList.push(11);
+myLinkedList.push(15);
 console.log(myLinkedList);
