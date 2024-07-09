342c95bc5fa81fe38e3ffb3387a86bf9a0f49297
Aniede Chibundu <ndubest56@gmail.com>
2024-07-09 14:20:37 +0100

    begin linked list redo

---
+++ UDEMY/01-linkedList.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    let newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+let myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
