2e7f39ffbf17339fea49661356474884983fcc86
Aniede Chibundu <ndubest56@gmail.com>
2024-07-13 06:45:33 +0100

    common ancesstor

---
+++ commonAncestor.js
@@ -0,0 +1,11 @@
+function commonAncestor(root, p, q) {
+  if (!root || root == p || root == q) return root;
+
+  let left = commonAncestor(root.left, p, q);
+  let right = commonAncestor(root.right, p, q);
+
+  if (!left) return right;
+  if (!right) return left;
+
+  return root;
+}
