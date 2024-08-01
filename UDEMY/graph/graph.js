2aa934a8e35e9453f8135ff006592d471b623082
Aniede Chibundu <ndubest56@gmail.com>
2024-08-01 10:51:40 +0100

    graph works

--- UDEMY/graph/graph.js
+++ UDEMY/graph/graph.js
@@ -1,12 +1,36 @@
 class Graph {
   constructor() {
-    this.adjacanceyList = {};
+    this.adjacencyList = {};
   }
   addNode(vertex) {
-    if (!this.adjacanceyList[vertex]) {
-      this.adjacanceyList[vertex] = [];
+    if (this.adjacencyList[vertex]) {
+      return false;
+    }
+    this.adjacencyList[vertex] = [];
+    return true;
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      // take care of duplicates
+      this.adjacencyList[vertex1] = [...new Set(this.adjacencyList[vertex1])];
+
+      this.adjacencyList[vertex2].push(vertex1);
+      // take care of duplicate
+      this.adjacencyList[vertex2] = [...new Set(this.adjacencyList[vertex2])];
       return true;
     }
     return false;
   }
 }
+
+let myGraph = new Graph();
+console.log(myGraph.addNode(`a`));
+console.log(myGraph.addNode(`b`));
+myGraph.addEdge(`a`, `b`);
+myGraph.addEdge(`a`, `b`);
+myGraph.addEdge(`a`, `b`);
+myGraph.addEdge(`a`, `b`);
+
+console.log(myGraph);
